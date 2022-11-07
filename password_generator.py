93af1066c3b2eea3e821707c5bb5299e8f336dfe
Roger Renjifo <rrrenjifo@gmail.com>
2022-11-07 15:37:05 -0400

    tarea numero 2 añadir menu

--- password_generator.py
+++ password_generator.py
@@ -1,19 +1,22 @@
 """password_generator"""
 import random
-def password_generator():
-    """generate an aleatory password"""
-    password = []
-    passw_leng = random.randint(9,16)
-    num_of_capital_letters= random.randint(1,(passw_leng-2))
-    num_of_lower_case_letters= random.randint(1,(passw_leng-num_of_capital_letters-1))
-    num_of_numbers= passw_leng - num_of_lower_case_letters - num_of_capital_letters
-    for _ in range(num_of_capital_letters):
-        password.append(chr(random.randint(65,90)))
-    for _ in range(num_of_lower_case_letters):
-        password.append(chr(random.randint(97,122)))
-    for _ in range(num_of_numbers):
-        password.append(str(random.randint(0,9)))
-    random.shuffle(password)
-    password_str= ''.join(password)
-    print ("la contraseña es:", password_str)
-password_generator()
+class PasswordGenerator:
+    """genera"""
+    def generate_password(self):
+        """generate an aleatory password"""
+        password = []
+        passw_leng = random.randint(9,16)
+        num_of_capital_letters= random.randint(1,(passw_leng-2))
+        num_of_lower_case_letters= random.randint(1,(passw_leng-num_of_capital_letters-1))
+        num_of_numbers= passw_leng - num_of_lower_case_letters - num_of_capital_letters
+        for _ in range(num_of_capital_letters):
+            password.append(chr(random.randint(65,90)))
+        for _ in range(num_of_lower_case_letters):
+            password.append(chr(random.randint(97,122)))
+        for _ in range(num_of_numbers):
+            password.append(str(random.randint(0,9)))
+        random.shuffle(password)
+        password_str= ''.join(password)
+        return password_str
+a=PasswordGenerator()
+print (a.generate_password())
